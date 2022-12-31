21bdd90c44e2a74fcdd76c29f279b3f224166832
Lauren Powers <laurenpowers@Laurens-Air.lan>
2022-12-31 10:06:15 -0500

    style under construction

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -3,7 +3,8 @@ import ship from "../images/ship.gif";
 function Home() {
   return (
     <>
-      <h1> Under Construction! Check back soon.</h1>
+      <h1> Under Construction! </h1>
+      <h1>Check back soon.</h1>
       <img src={ship} alt="ship" width={500} />
     </>
   );
