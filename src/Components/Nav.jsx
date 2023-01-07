e1acfb08b54dc7b47ac206ad41f0b6b331a1b6a5
Lauren Powers <laurenpowers@Laurens-Air.lan>
2023-01-07 16:30:22 -0500

    add active class and chang nav bar font

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -10,23 +10,23 @@ function Nav() {
           <NavLink className="navlink" to="/">
             Home <img src={anchor} className="nav-icon" alt="anchor icon" />
           </NavLink>
-          <NavLink className="navlink" to="/location">
+          <NavLink className="navlink" activeClassName="active" to="/location">
             Party
           </NavLink>
-          <NavLink className="navlink" to="/travel">
+          <NavLink className="navlink" activeClassName="active" to="/travel">
             Travel
           </NavLink>
-          <NavLink className="navlink" to="/faq">
+          <NavLink className="navlink" activeClassName="active" to="/faq">
             FAQs
           </NavLink>
-          <NavLink className="navlink" to="/ourstory">
+          <NavLink className="navlink" activeClassName="active" to="/ourstory">
             Our Story
           </NavLink>
 
-          <NavLink className="navlink" to="/photos">
+          <NavLink className="navlink" activeClassName="active" to="/photos">
             Photos
           </NavLink>
-          <NavLink className="navlink" to="/registry">
+          <NavLink className="navlink" activeClassName="active" to="/registry">
             Registry
           </NavLink>
         </div>
